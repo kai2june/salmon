29b64fdec7a2bbb961c796b2a46c740348b20d36
jimmy <fashion860312@gmail.com>
2021-06-22 22:20:22 +0800

    [FIX] GeneFileGenerator.hpp accounts for negative strand.

--- include/GeneFileGenerator.hpp
+++ include/GeneFileGenerator.hpp
@@ -141,6 +141,7 @@ class GeneFileGenerator
                               std::string outputGeneGff3FileName, 
                               std::string outputGeneTxpFastaFileName)
     {
+        /// @brief parse genome.fa positive strand
         std::ifstream ifs_genome(genomeFileName);
         std::map<std::string, std::string> genome;
         std::string current_chromosome, line;
@@ -156,6 +157,26 @@ class GeneFileGenerator
                 genome[current_chromosome] += line;
         }
         ifs_genome.close();
+        /// @brief reverse it to get negative strand of genome
+        std::map<std::string, std::string> genome_negative_strand;
+        for(auto iter=genome.begin(); iter!=genome.end(); ++iter)
+        {
+            std::cerr << "Generating complement strand of chromosome " << iter->first << std::endl;
+            genome_negative_strand[iter->first] = iter->second;
+            for(size_t i=0, sz = genome_negative_strand[iter->first].size(); i<sz; ++i)
+            {
+                char ch = genome_negative_strand[iter->first][i];
+                if(ch == 'A' || ch == 'a')
+                    genome_negative_strand[iter->first][i] += ('T' - 'A');
+                else if (ch == 'C' || ch == 'c')
+                    genome_negative_strand[iter->first][i] += ('G' - 'C');
+                else if (ch == 'G' || ch == 'g')
+                    genome_negative_strand[iter->first][i] += ('C' - 'G');
+                else if (ch == 'T' || ch == 't')
+                    genome_negative_strand[iter->first][i] += ('A' - 'T');
+            }
+        }
+
 
         // std::vector<GeneInfo> gene_info;
         std::ifstream ifs_geneMap(outputGeneGff3FileName);
@@ -175,6 +196,7 @@ class GeneFileGenerator
             gene_info_.back().chromosomeName = ll[0];
             gene_info_.back().start = stoi(ll[3]);
             gene_info_.back().end = stoi(ll[4]);
+            gene_info_.back().strand = ll[6][0];
             std::vector<std::string> ids;
             split(ll[8], ids, ';');
             for(auto elem : ids)
@@ -197,10 +219,19 @@ class GeneFileGenerator
         for(const auto& elem : gene_info_)
         {
             ofs_fasta << ">" << elem.geneName;
-            std::string s(genome[elem.chromosomeName].substr(elem.start-1, elem.end-elem.start+1));
+            std::string s;
+            if (elem.strand == '+')
+                s = genome[elem.chromosomeName].substr(elem.start-1, elem.end-elem.start+1);
+            else if (elem.strand == '-')
+            {
+                s = genome_negative_strand[elem.chromosomeName].substr(elem.start-1, elem.end-elem.start+1);
+                std::reverse(s.begin(), s.end());
+            }
+            else
+                std::cerr << ">>>>>>>>>>>>Error: strand should be either '+' or '-', not '" << elem.strand << "'" << std::endl;
             for(int32_t i(0); i<s.size(); ++i)
             {
-                if(i%60==0)
+                if(i%70==0)
                     ofs_fasta << "\n";
                 ofs_fasta << s[i];
             }
@@ -275,6 +306,7 @@ class GeneFileGenerator
         std::string geneName;
         int32_t start;
         int32_t end;
+        char strand;
     };
 
     std::vector<GeneInfo> gene_info_;
